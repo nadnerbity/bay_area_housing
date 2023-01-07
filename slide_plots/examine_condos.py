749b695f2ac4132f9ac89ecae43556e4591784bb
Brendan O'Shea <brendan@Crushinator.local>
2023-01-06 18:58:22 -0800

    Added histogram for condo affordability

--- slide_plots/examine_condos.py
+++ slide_plots/examine_condos.py
@@ -28,10 +28,10 @@ gdf = convert_df_to_gdf(df)
 # Rename the HOA column to get rid of the '/'
 gdf = gdf.rename(columns={'HOA/MONTH': 'HOAperMonth'})
 
+#
+# # This throws a warning that will need to be fixed.
+gdf = add_required_salary_to_dataframe(gdf, 0, 0.062/12, 0.0115/12, 0.01/12)
 
-# Keep only 2+2 or larger, we are looking for things for a family.
-gdf = gdf.loc[gdf['BEDS'] >= 2.0]
-gdf = gdf.loc[gdf['BATHS'] >= 2.0]
 
 # ------------------- PLOT 27.6 MINUTE DRIVE --------------------------
 data_name_to_load = 'slac'
@@ -47,38 +47,46 @@ time_to_contour = 60.0*27.6
 gpd_cont = generate_time_contours_from_grid(lng_grid, lat_grid, y_grid, time_to_contour)
 plot_gpd_boundary_on_map(gpd_cont, ax1, 'black')
 #
-# # Extract only houses within a 27.6 minute boundary of SLAC
-in_bdy = gdf[gdf.geometry.within(gpd_cont.geometry[0])]
 
-#
-# # This throws a warning that will need to be fixed.
-in_bdy = add_required_salary_to_dataframe(in_bdy, 0, 0.062/12, 0.0115/12, 0.01/12)
-#
-# scale_value = 1e6
-# plt.close(3456)
-# plt.figure(3456)
-# ax1 = plt.subplot(211)
-# plt.hist(in_bdy_28032022.required_salary.values,
-#          50,
-#          density=False,
-#          facecolor='r',
-#          alpha=0.75)
-# plt.xlim([0, 600])
-# plt.ylim([0, 22])
-# ax1.set_title('March 28th 2022', fontsize=18)
-#
-# ax2 = plt.subplot(212)
-# plt.hist(in_bdy_28112022.required_salary.values,
-#          50,
-#          density=False,
-#          facecolor='b',
-#          alpha=0.75)
-# plt.xlim([0, 600])
-# plt.ylim([0, 22])
-# ax2.set_title('November 28th 2022', fontsize=18)
-# ax2.set_xlabel('Required Yearly Salary [1000 $]', fontsize=18)
-#
-# plt.tight_layout()
+# (gdf.BATHS.values >= 2.0)
+# Keep only 2+2 or larger, we are looking for things for a family.
+# in_bdy = gdf.loc[(gdf.BATHS.values >= 2.0) & (gdf.BEDS.values >= 2.0)]
+
+# # Extract only houses within a 27.6 minute boundary of SLAC
+# in_bdy = in_bdy[in_bdy.geometry.within(gpd_cont.geometry[0])]
+
+OneAndOne = gdf.loc[(gdf.BATHS.values >= 1.0) & (gdf.BEDS.values >= 1.0)];
+OneAndOne = OneAndOne[OneAndOne.geometry.within(gpd_cont.geometry[0])]
+
+TwoAndTwo = gdf.loc[(gdf.BATHS.values >= 2.0) & (gdf.BEDS.values >= 2.0)]
+TwoAndTwo = TwoAndTwo[TwoAndTwo.geometry.within(gpd_cont.geometry[0])]
+
+
+scale_value = 1e6
+plt.close(3456)
+plt.figure(3456)
+ax1 = plt.subplot(211)
+plt.hist(OneAndOne.required_salary.values,
+         50,
+         density=False,
+         facecolor='r',
+         alpha=0.75)
+plt.xlim([0, 600])
+plt.ylim([0, 22])
+ax1.set_title('1+1 Condos', fontsize=18)
+
+ax2 = plt.subplot(212)
+plt.hist(TwoAndTwo.required_salary.values,
+         50,
+         density=False,
+         facecolor='b',
+         alpha=0.75)
+plt.xlim([0, 600])
+plt.ylim([0, 22])
+ax2.set_title('2+2 Condos', fontsize=18)
+ax2.set_xlabel('Required Yearly Salary [1000 $]', fontsize=18)
+
+plt.tight_layout()
 
 
 
